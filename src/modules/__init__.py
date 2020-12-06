2ea996fd74a045de288b89858f61695585f33f2c
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-12-07 00:38:35 +0100

    added game auto import added single/multi game reload function

--- src/modules/__init__.py
+++ src/modules/__init__.py
@@ -1,1 +1,37 @@
-from .tic_tac_toe import Main
+import importlib
+import importlib.util
+from pathlib import Path
+from types import ModuleType
+from typing import Dict
+
+from core import moduleUtils, utils
+from core.utils import placeHolderFunc
+
+__games: Dict[str, ModuleType] = {}
+
+
+def initializeGames():
+	"""	Initialize all games in the modules package	"""
+	for file in Path(__file__).parent.glob('*'):
+		if file.is_file() or file.name == '__pycache__':
+			continue
+		moduleName = f'modules.{file.name}'
+		module = importlib.import_module( moduleName )
+		__games[moduleName] = module
+
+
+def reloadGames():
+	"""	Reload all instatiated games """
+	for game in utils.copyList( __games.keys() ):
+		getattr(__games[game], 'reloadModules', placeHolderFunc)()
+		moduleUtils.reload(__games[game], __games)
+
+
+def reloadGame(name: str):
+	"""
+	Reload the game name
+	:param name: package/game to reload
+	"""
+	if name in __games.keys():
+		getattr( __games[ name ], 'reloadModules', placeHolderFunc )()
+		moduleUtils.reload(__games[name], __games)
