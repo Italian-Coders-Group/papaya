d52ff59a43f0435fbcd2cb2c59e4b4ea7ca5d772
Filip Aleksander Brzozowski <filipaleksander6060@gmail.com>
2021-01-23 22:45:12 +0100

    Some gameplay on TicTacToe, mostly logic for PVP, not done yet

--- src/modules/tic_tac_toe/Grid.py
+++ src/modules/tic_tac_toe/Grid.py
@@ -1,9 +1,10 @@
-from core.gridsystem.UnorderedGrid import BaseGrid
+# from core.gridsystem.UnorderedGrid import UnorderedGrid
 
 
-class Grid(BaseGrid):
+class Grid:
 
     def __init__(self, width: int, height: int):
         self.grid = [[None for _ in range(width)] for _ in range(height)]
+        print(self.grid)
 
 
