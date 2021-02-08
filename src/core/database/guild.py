bd6dc99311f0e9506012941dc0217d28d882a46d
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-02-08 18:21:11 +0100

    * takes long breath * refactored getGame: now only check the first result instead of all, updated for all PapGame properties, added explanation to SQL code refactored setGame: now fits all  properties in PapGame, added explaination to SQL code refactored hasGame: added optional parameter "gameType" to control the result list more, added SQL code explanation, less shitty game presence check refactored getGamesForUser: now actually gets games for a specific user, added SQL code explanation, now store preChecked games in an another list instead of iterating directly in them, now uses all the new properties aswell added getLiveGamesForUser(userID, gameType, user) method to get only live games from the database
    that was quite a lot : P
    
    Signed-off-by: ENDERZOMBI102 <enderzombi102.end@gmail.com>

--- src/core/database/guild.py
+++ src/core/database/guild.py
@@ -23,11 +23,14 @@ class Guild(AbstractGuild):
 		:return: the PapGame object
 		"""
 		if gameID not in self._gameCache.keys():
-			gameData: List[ Tuple ] = self.db.makeRequest('SELECT * FROM games WHERE guildID = ? AND gameID = ?', self.guildID, gameID )
-			self._gameCache[gameID ] = PapGame(
+			# EXPLANATION: select a game with gameID gameID and guildID guildID
+			gameData: Tuple = self.db.makeRequest('SELECT * FROM games WHERE guildID = ? AND gameID = ?', self.guildID, gameID )[0]
+			self._gameCache[ gameID ] = PapGame(
 				gameID=gameID,
-				userIDs=[ int(num) for num in gameData[0][2].split(',')  ],
-				gameData=json.loads( gameData[0][3] )
+				gameType=gameData[2],
+				userIDs=[ int(num) for num in gameData[3].split(',')  ],
+				gameData=json.loads( gameData[4] ),
+				live=gameData[5]
 			)
 		return self._gameCache.get( gameID )
 
@@ -38,13 +41,22 @@ class Guild(AbstractGuild):
 		"""
 		self._gameCache[ game.gameID ] = game
 		if self.hasGame( game.gameID, checkCache=False ):
-			self.db.makeRequest('DELETE FROM games WHERE guildID = ? AND gameID = ?', self.guildID, game.gameID )
+			self.db.makeRequest(
+				# EXPLANATION: delete a game
+				'DELETE FROM games WHERE guildID = ? AND gameID = ? AND gameType = ?',
+				self.guildID,
+				game.gameID,
+				game.gameType
+			)
 		self.db.makeRequest(
-			'INSERT INTO games (guildID, gameID, userIDs, gameData) VALUES (?, ?, ?, ?)',
+			# EXPLANATION: insert a game with all thir values
+			'INSERT INTO games (guildID, gameID, gameType, userIDs, gameData, live) VALUES (?, ?, ?, ?, ?, ?)',
 			self.guildID,
 			game.gameID,
+			game.gameType,
 			str( game.userIDs )[1:][:-1].replace(' ', ''),
-			json.dumps( game.gameData, indent=None, separators=(',', ':') )
+			json.dumps( game.gameData, indent=None, separators=(',', ':') ),
+			game.live
 		)
 		self.db.save()
 
@@ -56,23 +68,31 @@ class Guild(AbstractGuild):
 		"""
 		raise NotImplementedError()
 
-	def hasGame( self, gameID: str, checkCache: bool = True ) -> bool:
+	def hasGame( self, gameID: str, checkCache: bool = True, gameType: Optional[ str ] = 'any' ) -> bool:
 		"""
 		Checks if has a game with that ID
 		:param gameID: the game ID to search for
 		:param checkCache: True if should check the cache too
+		:param gameType: the type of the game to search, optional, can make searching faster
 		:return: True if we have it
 		"""
 		if checkCache:
 			if gameID in self._gameCache.keys():
 				return True
-		return True in [
-			gameID == x[0] for x in self.db.makeRequest(
-				'SELECT * FROM games WHERE guildID = ? AND gameID = ?',
-				self.guildID,
-				gameID
-			)
-		]
+		games: list = self.db.makeRequest(
+			# EXPLANATION: select all games that are from this guild, are of type gameType and
+			# their id is gameID
+			'SELECT * FROM games WHERE guildID = ? AND gameID = ? AND gameType = ?',
+			self.guildID,
+			gameID
+		) if gameType != 'any' else self.db.makeRequest(
+			# EXPLANATION: select all games that are from this guild and their id is gameID
+			'SELECT * FROM games WHERE guildID = ? AND gameID = ?',
+			self.guildID,
+			gameID,
+			gameType
+		)
+		return len( games ) > 0
 
 	def hasUser( self, userID: int ) -> bool:
 		"""
@@ -82,22 +102,72 @@ class Guild(AbstractGuild):
 		"""
 		raise NotImplementedError()
 
-	def getGamesForUser( self, userID: int, user: Optional[PapUser] = None ) -> List[PapGame]:
+	def getGamesForUser( self, userID: int, gameType: str = 'any', user: Optional[PapUser] = None ) -> List[PapGame]:
 		"""
 		Returns a list with all games that this user has played
+		:param gameType: the type of the game, use "any" for any type
 		:param userID: the user to get the games from, put None if use use a PapUser in the user param
 		:param user: ALTERNATIVE: a PapUser
 		:return: list of games
 		"""
 		games = []
-		for game in self.db.makeRequest( 'SELECT * FROM games WHERE guildID = ?', self.guildID ):
-			if str(userID) in game[2].split(','):
+		dbGames: list = self.db.makeRequest(
+			# EXPLANATION: select all games that are from this guild, have the type ?,
+			# and include userID in userIDs
+			'SELECT * FROM games WHERE guildID = ? AND gameType = ? AND userIDs LIKE ?',
+			self.guildID,
+			gameType,
+			f'%{userID}%'
+		) if gameType != 'any' else self.db.makeRequest(
+			# EXPLANATION: select all games that are from this guild and include userID in userIDs
+			'SELECT * FROM games WHERE guildID = ? AND userIDs LIKE ?',
+			self.guildID,
+			f'%{userID}%'
+		)
+		for game in dbGames:
+			if str(userID) in game[3].split(','):
 				games.append(
 					PapGame(
 						gameID=game[1],
-						userIDs=[ int( num ) for num in game[ 2 ].split( ',' ) ],
-						gameData=json.loads( game[ 3 ] )
+						live=bool( game[5] ),
+						gameType=game[2],
+						userIDs=[ int( num ) for num in game[ 3 ].split( ',' ) ],
+						gameData=json.loads( game[ 4 ] )
 					)
 				)
 		return games
 
+	def getLiveGameForUser( self, userID: int, gameType: str = 'any', user: Optional[ PapUser ] = None ) -> List[ PapGame ]:
+		"""
+		Returns a list with all live games that this user is playing
+		:param gameType: the type of the game, use "any" for any type
+		:param userID: the user to get the games from, put None if use use a PapUser in the user param
+		:param user: ALTERNATIVE: a PapUser
+		:return: list of games
+		"""
+		games = [ ]
+		dbGames: list = self.db.makeRequest(
+			# EXPLANATION: select all games that are live, from this guild, are of the type gameType,
+			# and include userID in userIDs
+			'SELECT * FROM games WHERE live = 1 AND guildID = ? AND gameType = ? AND userIDs LIKE ?',
+			self.guildID,
+			gameType,
+			f'%{userID}%'
+		) if gameType != 'any' else self.db.makeRequest(
+			# EXPLANATION: select all games that are live, from this guild and include userID in userIDs
+			'SELECT * FROM games WHERE live = 1 AND guildID = ? AND userIDs LIKE ?',
+			self.guildID,
+			f'%{userID}%'
+		)
+		for game in dbGames:
+			if str( userID ) in game[ 3 ].split( ',' ):
+				games.append(
+					PapGame(
+						gameID=game[ 1 ],
+						live=bool( game[ 5 ] ),
+						userIDs=[ int( num ) for num in game[ 3 ].split( ',' ) ],
+						gameData=json.loads( game[ 4 ] ),
+						gameType=game[2]
+					)
+				)
+		return games
