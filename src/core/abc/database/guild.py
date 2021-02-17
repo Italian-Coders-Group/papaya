b1309ac10a1e64adff2560ec181c6d3d3ecbb32b
Filip Aleksander Brzozowski <filipaleksander6060@gmail.com>
2021-02-17 23:03:15 +0100

    added method getStatsForUserInGuild

--- src/core/abc/database/guild.py
+++ src/core/abc/database/guild.py
@@ -81,3 +81,13 @@ class AbstractGuild(metaclass=ABCMeta):
 		:return: list of games
 		"""
 		pass
+
+	@abstractmethod
+	def getStatsForUserInGuild(self, userID: int, guildID: str, gameType: str = 'any') -> PapUser:
+		"""
+		Returns a user in the guild with his stats, None if not found
+		:param userID:
+		:param guildID:
+		:param gameType:
+		:return: user
+		"""
