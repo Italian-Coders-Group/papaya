86bbf4266ccc51dea69b9f7f7347a2d31bf1b3d8
Filip Aleksander Brzozowski <filipaleksander6060@gmail.com>
2021-04-20 20:41:10 +0200

    new Player ABC

---
+++ src/core/abc/games/player.py
@@ -0,0 +1,8 @@
+from abc import ABCMeta, abstractmethod
+from discord import Member
+from os import path
+
+
+# I have no idea what to put in this abc tbh :/
+class Player(metaclass=ABCMeta):
+	pass
