48f6eef9b378d6ccc99825d27eba8b00f541299c
Filip Aleksander Brzozowski <filipaleksander6060@gmail.com>
2021-04-25 21:19:14 +0200

    implement player makeMove() + abstractmethod

--- src/core/abc/games/player.py
+++ src/core/abc/games/player.py
@@ -1,8 +1,18 @@
 from abc import ABCMeta, abstractmethod
 from discord import Member
 from os import path
+from typing import Dict
 
 
 # I have no idea what to put in this abc tbh :/
 class Player(metaclass=ABCMeta):
-	pass
+
+	@abstractmethod
+	def makeMove(self, data: Dict) -> int:
+		"""
+		This function returns a move number to be interpeted by the game.
+
+		:param data:
+		:return:
+		"""
+		pass
---
+++ src/modules/tic_tac_toe/TicTacToePlayer.py
@@ -0,0 +1,73 @@
+import os
+from core.abc.games.player import Player
+from PIL import Image
+from discord import Member
+from typing import Dict
+
+
+def convertCoords(inputValue: str):
+
+	possible_coords = {
+		'tl': (0, 0),
+		'11': (0, 0),
+		'topleft': (0, 0),
+		't': (0, 1),
+		'top': (0, 1),
+		'12': (0, 1),
+		'tr': (0, 2),
+		'topright': (0, 2),
+		'13': (0, 2),
+		'ml': (1, 0),
+		'middleleft': (1, 0),
+		'midleft': (1, 0),
+		'21': (1, 0),
+		'mid': (1, 1),
+		'middle': (1, 1),
+		'22': (1, 1),
+		'mr': (1, 2),
+		'middleright': (1, 2),
+		'midright': (1, 2),
+		'23': (1, 2),
+		'bl': (2, 0),
+		'bottomleft': (2, 0),
+		'31': (2, 0),
+		'bottom': (2, 1),
+		'b': (2, 1),
+		'32': (2, 1),
+		'br': (2, 2),
+		'bottomright': (2, 2),
+		'33': (2, 2)
+	}
+
+	for key in possible_coords.keys():
+		if inputValue == key:
+			return possible_coords[key]
+
+	return None, None
+
+
+class TicTacToePlayer(Player):
+
+	def __init__(self, user: Member, symbol: Image, sign: str):
+		self.user = user,
+		self.symbol = symbol
+		self.sign = sign
+
+	def makeMove(self, data: Dict):
+		grid = data['grid']
+		coords = data['coords']
+
+		posX, posY = convertCoords(coords)
+
+		if (posX is None) and (posY is None):
+			return grid, 3
+
+		if grid[posY][posX] == '':
+			grid[posY][posX] = self.sign
+		else:
+			pass
+
+		return grid, 0
+
+	def getUser( self ):
+		return self.user
