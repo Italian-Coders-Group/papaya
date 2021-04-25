4eafa445e4e936319f044cdbf9f811adee2bbb7d
Filip Aleksander Brzozowski <filipaleksander6060@gmail.com>
2021-04-25 22:30:53 +0200

    api run in separate thread

--- src/PapayaBot.py
+++ src/PapayaBot.py
@@ -2,8 +2,10 @@ import os
 import sys
 
 import core
+from src.api import createApp
 
 
+createApp()
 core.Bot().initLoggingAndRun(
 	filename='../logs/latest.log',
 	token=os.environ.get( 'TOKEN_TEST' if '--test' in sys.argv else 'TOKEN' )
