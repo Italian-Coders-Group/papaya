5b4b263b6b331df1fe56c869f8cbcd9d40ea343b
Filip Aleksander Brzozowski <filipaleksander6060@gmail.com>
2021-05-03 18:54:38 +0200

    removed API as it's not necessary yet.

--- src/PapayaBot.py
+++ src/PapayaBot.py
@@ -2,10 +2,10 @@ import os
 import sys
 
 import core
-from src.api import createApp
+# from src.api import createApp
 
 
-createApp()
+# createApp() REMOVING API, NO NEED FOR IT NOW
 core.Bot().initLoggingAndRun(
 	filename='../logs/latest.log',
 	token=os.environ.get( 'TOKEN_TEST' if '--test' in sys.argv else 'TOKEN' )
